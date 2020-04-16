04de91afc4b3797f34f20def06677fb05bac190a
Abdelaziz <abdelaziz.iddouch@gmail.com>
2020-04-16 22:46:19 +0100

    fixed ingress-service problem

--- client/src/App.js
+++ client/src/App.js
@@ -6,31 +6,36 @@ import Fib from './Fib';
 import AnotherPage from './AnotherPage';
 
 function App() {
-  return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello Kubernetes !!!!!!!!!!!!</p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-          <Link to="/">Home</Link>
-          <Link to="/anotherPage">Other page</Link>
-        </header>
-        <div>
-          <Switch>
-            <Route exact path="/">
-              <Fib />
-            </Route>
-            <Route path="/anotherPage">
-              <AnotherPage />
-            </Route>
-          </Switch>
-        </div>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<p>Hello Kubernetes !!!!!!!!!!!!</p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+					</a>
+					<Link to="/">Home</Link>
+					<Link to="/anotherPage">Other page</Link>
+				</header>
+				<div>
+					<Switch>
+						<Route exact path="/">
+							<Fib />
+						</Route>
+						<Route path="/anotherPage">
+							<AnotherPage />
+						</Route>
+					</Switch>
+				</div>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
--- client/src/Fib.js
+++ client/src/Fib.js
@@ -2,63 +2,74 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function Fib() {
-  const [seenIndexes, setSeenIndexes] = useState([]);
-  const [values, setValues] = useState({});
-  const [index, setIndex] = useState('');
+	const [seenIndexes, setSeenIndexes] = useState([]);
+	const [values, setValues] = useState({});
+	const [index, setIndex] = useState('');
 
-  useEffect(() => {
-    // fetchIndexes();
-    fetchValues();
-  }, []);
+	useEffect(() => {
+		fetchIndexes();
+		fetchValues();
+	}, []);
 
-  async function fetchValues() {
-    const values = await axios.get('/api/values/current');
-    setValues(values.data);
-  }
+	async function fetchValues() {
+		try {
+			const values = await axios.get('/api/values/current');
+			setValues(values.data);
+		} catch (err) {
+			console.error("fetchValues() didn't reach the server!!!");
+			console.log(err);
+		}
+	}
 
-  async function fetchIndexes() {
-    const indexes = await axios.get('/api/values/all');
-    setSeenIndexes(indexes.data);
-  }
+	async function fetchIndexes() {
+		try {
+			const indexes = await axios.get('/api/values/all');
+			setSeenIndexes(indexes.data);
+		} catch (err) {
+			console.error("fetchIndexes() didn't reach the server!!!");
+			console.log(err);
+		}
+	}
 
-  function renderSeenIndexes() {
-    return seenIndexes.map(({ number }) => number).join(', ');
-  }
+	function renderSeenIndexes() {
+		return seenIndexes.map(({ number }) => number).join(', ');
+	}
 
-  function renderValues() {
-    const entries = [];
-    for (let key in values) {
-      entries.push(
-        <div key={key}>
-          For index {key} i calculated {values[key]}
-        </div>
-      );
-    }
+	function renderValues() {
+		const entries = [];
+		for (let key in values) {
+			entries.push(
+				<div key={key}>
+					For index {key} i calculated {values[key]}
+				</div>
+			);
+		}
 
-    return entries;
-    // console.log(entries);
-  }
+		return entries;
+	}
 
-  async function handleSubmit(e) {
-    e.preventDefault();
+	async function handleSubmit(e) {
+		e.preventDefault();
+		try {
+			await axios.post('/api/values', {
+				index: index,
+			});
+			setIndex('');
+		} catch (err) {
+			console.log(err);
+		}
+	}
 
-    await axios.post('/api/values', {
-      index: index
-    });
-
-    setIndex('');
-  }
-
-  return (
-    <div>
-      <form onSubmit={e => handleSubmit(e)}>
-        <input value={index} onChange={e => setIndex(e.target.value)} />
-        <button>Submit</button>
-      </form>
-      <h3>Indexes i have seen :</h3>
-      {renderSeenIndexes()}
-      <h3>Calulated values :</h3>
-      {renderValues()}
-    </div>
-  );
+	return (
+		<div>
+			<form onSubmit={(e) => handleSubmit(e)}>
+				<input value={index} onChange={(e) => setIndex(e.target.value)} />
+				<button>Submit</button>
+			</form>
+			<h3>Indexes i have seen :</h3>
+			{renderSeenIndexes()}
+			<h3>Calulated values :</h3>
+			{renderValues()}
+		</div>
+	);
 }
